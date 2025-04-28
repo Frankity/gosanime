5f5e1e4f30036a8eea11458fae4c638d957da221
Douglas Brunal <frankithy@gmail.com>
2025-04-28 08:07:10 -0500

    fixed video servers parsing

--- main/server/videos.go
+++ main/server/videos.go
@@ -1,15 +1,12 @@
 package server
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
+	"regexp"
 
 	"github.com/anaskhan96/soup"
 	"xyz.frankity/gosanime/main/config"
@@ -20,7 +17,7 @@ import (
 func videosByServer(r *http.Request) (interface{}, error) {
 	var err error
 	if err := r.ParseForm(); err != nil {
-		os.Exit(1)
+		return nil, fmt.Errorf("error parsing form: %v", err)
 	}
 	anime := r.Form.Get("anime")
 	episode := r.Form.Get("episode")
@@ -36,106 +33,43 @@ func videosByServer(r *http.Request) (interface{}, error) {
 
 	doc := soup.HTMLParse(responseString)
 
-	urls := []string{}
-	for _, in := range doc.FindAll("script") {
-		if strings.Contains(in.Text(), "var video = [];") {
-			d := in.Children()[0]
-			arr := strings.Split(d.NodeValue, "\n")
-			for i := 0; i < len(arr); i++ {
-				if strings.Contains(arr[i], "player_conte") {
-
-					html := soup.HTMLParse(arr[i])
-					urli := html.Find("iframe", "class", "player_conte").Attrs()["src"]
-
-					if strings.Contains(urli, "jk.php") {
-						ur := strings.Replace(urli, "/jk.php?u=", "https://jkanime.net/", -1)
-						urls = append(urls, GetHiddenUrl(ur))
-						break
-					}
-
-					doc, err := soup.Get(urli)
-
-					if err != nil {
-						fmt.Print(err)
-					}
-
-					datas := soup.HTMLParse(doc)
-
-					if strings.Contains(datas.HTML(), "input") {
-
-						redirUrl := datas.FindAll("input")[0].Attrs()["value"]
-
-						data := url.Values{}
-						data.Set("data", redirUrl)
-
-						client := &http.Client{}
-						r, err := http.NewRequest("POST", "https://jkanime.net/gsplay/redirect_post.php", strings.NewReader(data.Encode())) // URL-encoded payload
-						if err != nil {
-							log.Fatal(err)
-						}
-						r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-						r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-
-						res, err := client.Do(r)
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						defer res.Body.Close()
-
-						if err != nil {
-							log.Fatal(err)
-						}
-
-						vUrl := strings.Split(string(fmt.Sprint(res.Request)), " ")[1]
-
-						if strings.Contains(vUrl, "#") {
-							hash := strings.Split(vUrl, "#")
-
-							d := url.Values{}
-							d.Set("v", hash[1])
-
-							p, err := http.PostForm("https://jkanime.net/gsplay/api.php", url.Values{"v": {hash[1]}})
-							if err != nil {
-								log.Fatal(err)
-							}
-
-							r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-							r.Header.Add("Content-Length", strconv.Itoa(len(d.Encode())))
-
-							if nil != err {
-								fmt.Println("error in action happened getting the response", err)
-							}
-
-							defer p.Body.Close()
-							body, err := ioutil.ReadAll(p.Body)
-
-							if nil != err {
-								fmt.Println("error in acion happened reading the body", err)
-							}
-
-							var url Url
-							_ = json.Unmarshal([]byte(body), &url)
-
-							urls = append(urls, url.File)
-
-						} else {
-							urls = append(urls, *&vUrl)
-						}
-					}
-				}
-			}
+	var serversJSON string
+	scriptRegex := regexp.MustCompile(`var servers = (\[.*?\]);`)
+	for _, script := range doc.FindAll("script") {
+		matches := scriptRegex.FindStringSubmatch(script.Text())
+		if len(matches) > 1 {
+			serversJSON = matches[1]
+			break
 		}
+	}
+
+	if serversJSON == "" {
+		return nil, fmt.Errorf("no se encontró el JSON de servidores")
+	}
+
+	var serversData []struct {
+		Remote string `json:"remote"`
+		Server string `json:"server"`
+		Lang   int    `json:"lang"`
+		Size   string `json:"size"`
+	}
+	if err := json.Unmarshal([]byte(serversJSON), &serversData); err != nil {
+		return nil, fmt.Errorf("error al decodificar el JSON de servidores: %v", err)
 	}
 
 	servers := []models.Server{}
-	for i := 0; i < len(urls); i++ {
-		s := models.Server{
-			Name: fmt.Sprintf("Servidor %v", i+1),
-			Url:  urls[i],
+	for _, server := range serversData {
+		decodedURL, err := base64.StdEncoding.DecodeString(server.Remote)
+		if err != nil {
+			log.Printf("error al decodificar la URL en Base64: %v", err)
+			continue
 		}
-		servers = append(servers, s)
+
+		servers = append(servers, models.Server{
+			Name: fmt.Sprintf("%s (%s)", server.Server, server.Size),
+			Url:  string(decodedURL),
+		})
 	}
 
-	return servers, err
+	return servers, nil
 }
